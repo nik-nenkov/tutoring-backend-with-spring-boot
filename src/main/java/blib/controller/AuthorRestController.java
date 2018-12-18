3b0ac91390fadcfd9fd93fb6238ebb9a57a1b8be
Nikolay Nenkov <Nikolay_Nenkov@epam.com>
2018-12-18 16:38:04 +0200

    working version

--- src/main/java/blib/controller/AuthorRestController.java
+++ src/main/java/blib/controller/AuthorRestController.java
@@ -2,34 +2,30 @@ package blib.controller;
 
 import blib.dao.AuthorRepository;
 import blib.model.Author;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
-
 @RestController
 public class AuthorRestController {
-    private final AuthorRepository authRepository;
+  private final AuthorRepository authRepository;
 
-    @Autowired
-    public AuthorRestController(AuthorRepository authRepository) {
-        this.authRepository = authRepository;
+  @Autowired
+  public AuthorRestController(AuthorRepository authRepository) {
+    this.authRepository = authRepository;
+  }
+
+  @CrossOrigin(origins = "*")
+  @GetMapping(value = "/authors", produces = "application/json")
+  // READ points:
+  public List<Author> suggestAuth(@RequestParam(value = "s", defaultValue = "") String sampleName) {
+    List<Author> sample = null;
+    if (sampleName.length() >= 1) {
+      sample = authRepository.findAllByNameContaining(sampleName);
     }
-
-    @RequestMapping(
-            value = "/authors",
-            produces = "application/json")
-    //READ points:
-    public List<Author> suggestAuth(@RequestParam(value = "s", defaultValue = "") String sampleName) {
-        List<Author> sample = null;
-        if (sampleName.length() >= 1) {
-            sample = authRepository.findAllByNameContaining(sampleName);
-        }
-        return sample;
-    }
-
-
+    return sample;
+  }
 }
